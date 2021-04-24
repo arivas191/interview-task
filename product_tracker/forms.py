093c168a6c326d9a7e8a9e04dd4ce712a7328cdd
Augusto Rivas <rivasaugusto10@gmail.com>
2021-04-24 17:50:25 -0400

    Added the analytics page and file upload for the product's image

--- product_tracker/forms.py
+++ product_tracker/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, DecimalField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length, ValidationError
 
@@ -8,4 +9,5 @@ class AddProductForm(FlaskForm):
     product_description = StringField('Description', validators=[DataRequired(), Length(min=5, max=100)])
     price = DecimalField('Price (in US dollars)', validators=[DataRequired()])
     category = SelectField('Category', validators=[DataRequired()])
+    image = FileField('Add Product Image', validators=[FileAllowed(['jpg', 'png', 'jpeg', 'pdf'])])
     add = SubmitField('Add Product')
--- product_tracker/routes.py
+++ product_tracker/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, url_for, flash, redirect, request, session
+from flask import render_template, url_for, redirect, request, session, json, current_app
 from product_tracker import app, mysql
 from product_tracker.forms import AddProductForm
+import secrets, os
 
 # Endpoint for getting products, either by category or all of them
 @app.route('/products')
@@ -21,6 +22,7 @@ def products(category=None):
             cursor.close()
             return render_template('error.html')
     products = cursor.fetchall()
+
     cursor.close()
     return render_template('products.html', products=products, category=category)
 
@@ -55,12 +57,15 @@ def add_product():
     form = AddProductForm()
     form.category.choices = categories
     if form.validate_on_submit():
+        file_name = ''
+        if form.image.data:
+            file_name = save_image(form.image.data)
         try:
             cursor.execute("INSERT INTO "
-                           "Product (Name, Description, Price, CategoryId) "
-                           "VALUES (%s, %s, %s, %s)", 
+                           "Product (Name, Description, Price, CategoryId, Image) "
+                           "VALUES (%s, %s, %s, %s, %s)", 
                            (form.product_name.data, form.product_description.data, 
-                           form.price.data, form.category.data))
+                           form.price.data, form.category.data, file_name))
         except BaseException as error:
             print(error)
             cursor.close()
@@ -70,9 +75,53 @@ def add_product():
         return redirect(url_for('products'))
     return render_template('add_product.html', form=form)
 
+# Helper method for storing the image in the static folder of the project
+def save_image(image):
+    random_hex = secrets.token_hex(8)
+    _, extension = os.path.splitext(image.filename)
+    file_name = random_hex + extension
+    image_path = os.path.join(current_app.root_path, 'static/product_images', file_name)
+    image.save(image_path)
+
+    return file_name
+
 # Endpoint for getting the analytics
-def analytics():
+@app.route('/analytics')
+@app.route('/analytics/<category>')
+def analytics(category='fruits'):
+    connection = mysql.connect()
+    cursor = connection.cursor()
     
+    # Get the available categories to display them in a dropdown menu
+    categories_results = get_categories(cursor)
+    categories = []
+    for result in categories_results:
+        categories.append(result['Name'])
+
+    labels, data = get_category_sales(cursor, category)
+    return render_template('analytics.html', labels=json.dumps(labels), data=json.dumps(data), 
+                            categories=categories, current_category=category)
+
+# Helper method for getting the total sales for each product in every category
+def get_category_sales(cursor, category):
+    cursor.execute("SELECT pdt.Name AS Product, "
+                   "TRUNCATE(SUM(pch.Price*pch.Quantity), 2) AS Sales "
+                   "FROM Category cat "
+                   "INNER JOIN Product pdt ON cat.Id = pdt.CategoryId "
+                   "INNER JOIN Purchase pch ON pdt.Id = pch.ProductId "
+                   "WHERE cat.Name = %s"
+                   "GROUP BY pch.ProductId", (category))
+    results = cursor.fetchall()
+    # Create two arrays for the chart, one for the lables (product names) and one for the
+    # chart's data (product sales)
+    product_labels = []
+    product_data = []
+    for result in results:
+        product_labels.append(result['Product'])
+        product_data.append(result['Sales'])
+    
+    return product_labels, product_data
+
 #Helper method for getting the available categories from the DB
 def get_categories(cursor):
     cursor.execute("SELECT * FROM Category")
